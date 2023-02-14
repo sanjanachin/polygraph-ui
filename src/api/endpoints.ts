533dd42e4275ddd06a6d760899007e8ea6694eb9
clynch20 <clynch20@uw.edu>
2023-02-14 12:12:11 -0800

    updated temp endpoints for beta

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,6 +1,10 @@
 const BASE_URL = 'https://homes.cs.washington.edu/~naira2/403testendpoint';
-const MISINFORMATION_URL = '/misinformationCheckEndpoint.php';
+// const MISINFORMATION_URL = '/misinformationCheckEndpoint.php';
 const HISTORY_URL = '';
 
-export const MISINFORMATION_CHECK_ENDPOINT = BASE_URL + MISINFORMATION_URL;
+const BACKEND_TEMP_URL = 'http://127.0.0.1:5000';
+const TEMP_MISIN_URL = '/model';
+
+// export const MISINFORMATION_CHECK_ENDPOINT = BASE_URL + MISINFORMATION_URL;
+export const MISINFORMATION_CHECK_ENDPOINT = BACKEND_TEMP_URL + TEMP_MISIN_URL;
 export const HISTORY_ENDPOINT = BASE_URL + HISTORY_URL;
