a0ba02fc79b50c8b86a3e9a911f0769cb6aafcae
clynch20 <clynch20@uw.edu>
2023-02-14 13:15:50 -0800

    coalesced backend endpoints to one url

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -3,7 +3,7 @@ const BASE_URL = 'https://homes.cs.washington.edu/~naira2/403testendpoint';
 const HISTORY_URL = '';
 
 const BACKEND_TEMP_URL = 'http://127.0.0.1:5000';
-const TEMP_MISIN_URL = '/model';
+const TEMP_MISIN_URL = '/misinformation';
 
 // export const MISINFORMATION_CHECK_ENDPOINT = BASE_URL + MISINFORMATION_URL;
 export const MISINFORMATION_CHECK_ENDPOINT = BACKEND_TEMP_URL + TEMP_MISIN_URL;
