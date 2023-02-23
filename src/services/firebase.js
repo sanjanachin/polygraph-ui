9f868bf8cc98b4f58a1de3e0001cea5e01be8850
Ayan Gupta <74832088+ayangupt@users.noreply.github.com>
2023-02-23 13:46:17 -0800

    Removing key

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,13 +8,7 @@ import { getFirestore, collection, addDoc } from 'firebase/firestore/lite';
 import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDirvxTGs-Tk5lVPUzqJINb56kA6nd1n-c',
-  authDomain: 'polygraph-792ef.firebaseapp.com',
-  projectId: 'polygraph-792ef',
-  storageBucket: 'polygraph-792ef.appspot.com',
-  messagingSenderId: '909822764908',
-  appId: '1:909822764908:web:e5010f4c32289c9c0f7549',
-  measurementId: 'G-NCN4WHS511',
+// Ommited for security reasons. Will update later. 
 };
 
 // Initialize Firebase
